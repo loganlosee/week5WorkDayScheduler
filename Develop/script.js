3367ac4b210485d76b4d8b7666f17fc9c3bad7d0
Logan Losee <logan.losee99@gmail.com>
2023-09-24 17:48:13 -0600

    created js loops for creating html containers and checking current time

--- Develop/script.js
+++ Develop/script.js
@@ -1,34 +1,53 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+$(document).ready(function () {
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-
-  $(function () {
-    // Add a click listener to all save buttons
-  $(".saveBtn").on("click", function () {
-    // Get the hour of the parent time-block
-    var timeBlockHour = $(this).closest(".time-block").attr("id");
-    // Get the input from the textarea
-    var input = $(this).siblings("textarea").val();
-    //I used this to verify function is working
-    console.log(timeBlockHour, input)
-    // Save the input in local storage using the timeBlockHour as the key
-    localStorage.setItem(timeBlockHour, input);
-  });
-
-
+     // Get the current hour using dayjs
+    var currentHour = 12 //dayjs().hour();
   
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+    // Select the time block container by class
+    var timeBlockContainer = document.querySelector(".container-lg.px-5");
+  
+    // Loop from 9 AM (hour 9) to 5 PM (hour 17)
+    for (var hour = 9; hour <= 17; hour++) {
+      // Create a div element for the time block
+      var timeBlock = document.createElement("div");
+      timeBlock.id = "hour-" + hour;
+      timeBlock.className = "row time-block";
+  
+      // Determine whether it's past, present, or future
+      if (hour < currentHour) {
+        timeBlock.className += " past";
+      } else if (hour === currentHour) {
+        timeBlock.className += " present";
+      } else {
+        timeBlock.className += " future";
+      }
+  
+      // Create the HTML structure for the time block (copied from starter code)
+      timeBlock.innerHTML = `
+        <div class="col-2 col-md-1 hour text-center py-3">${hour}AM</div>
+        <textarea class="col-8 col-md-10 description" rows="3"></textarea>
+        <button class="btn saveBtn col-2 col-md-1" aria-label="save">
+          <i class="fas fa-save" aria-hidden="true"></i>
+        </button>
+      `;
+  
+      // Append the time block to the container
+      timeBlockContainer.appendChild(timeBlock);
+    }
+    // Add a click listener to all save buttons
+    $(".saveBtn").on("click", function () {
+      // Get the hour of the parent time-block
+      var timeBlockHour = $(this).closest(".time-block").attr("id");
+      // Get the input from the textarea
+      var input = $(this).siblings("textarea").val();
+      
+      // Log to check if the click event is triggered and for debugging
+      console.log("Button Clicked:", timeBlockHour, input);
+      
+      // Save the input in local storage using the timeBlockHour as the key
+      localStorage.setItem(timeBlockHour, input);
+    });    
+  });
 
   
   
@@ -41,4 +60,4 @@
   // TODO: Add code to display the current date in the header of the page.
 
 
-});
+
